52b02a4c767f75999c8db48ca8bc4b5b603db1d3
Daniil Sydorov <daniil.sydorov@nure.ua>
2026-06-15 18:47:32 +0300

    Delete Lab1/LabWork1 directory

--- Lab1/LabWork1/rates/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Currency
-
-class CurrencyForm(forms.ModelForm):
-    class Meta:
-        model = Currency
-        fields = ['name', 'buy_rate', 'sell_rate']
--- Lab1/LabWork1/rates/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Currency(models.Model):
-    name = models.CharField(max_length=50)
-    buy_rate = models.DecimalField(max_digits=10, decimal_places=2)
-    sell_rate = models.DecimalField(max_digits=10, decimal_places=2)
