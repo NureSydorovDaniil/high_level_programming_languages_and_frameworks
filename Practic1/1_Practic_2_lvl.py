9eb3f780e851a0b38b428efadc73a5eae4029b6f
Daniil Sydorov <daniil.sydorov@nure.ua>
2026-06-15 17:30:29 +0300

    Add files via upload

---
+++ Practic1/1_Practic_2_lvl.py
@@ -0,0 +1,6 @@
+numbers = list(range(1, 21))
+for number in numbers:
+    print(f"Число: {number} | Квадрат числа: {number ** 2}")
+
+
+    
---
+++ Practic1/1_Practic_3_lvl.py
@@ -0,0 +1,7 @@
+def get_numbers(numbers):
+    return [number for number in numbers if number % 2 == 0]
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+print(get_numbers(numbers))
+
+
